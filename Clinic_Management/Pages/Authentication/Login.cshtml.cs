0efea561c7b716cc8fd203ea6dfb7daff719ab13
donaldDTrumppp <qtrung1412003@gmail.com>
2024-07-16 14:31:13 +0700

    .

--- Clinic_Management/Pages/Authentication/Login.cshtml.cs
+++ Clinic_Management/Pages/Authentication/Login.cshtml.cs
@@ -1,19 +1,28 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Clinic_Management.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
 
 namespace Clinic_Management.Pages.Authentication
 {
     public class LoginModel : PageModel
     {
         private readonly G1_PRJ_DBContext _context;
+        private readonly IConfiguration _configuration;
 
-        public LoginModel(G1_PRJ_DBContext context)
+        public LoginModel(G1_PRJ_DBContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         [BindProperty]
@@ -29,7 +38,6 @@ namespace Clinic_Management.Pages.Authentication
 
         public void OnGet()
         {
-
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -49,8 +57,48 @@ namespace Clinic_Management.Pages.Authentication
                 return Page();
             }
 
+            var role = await _context.Roles
+                .Where(r => r.RoleId == user.RoleId)
+                .Select(r => r.RoleName)
+                .FirstOrDefaultAsync();
 
+            if (role == null)
+            {
+                ErrorMessage = "User role not found.";
+                return Page();
+            }
+
+            var token = GenerateJwtToken(user, role);
+
+            
+            HttpContext.Session.SetString("JWTToken", token);
+
+            
             return RedirectToPage("/Home/Home");
+        }
+
+        private string GenerateJwtToken(User user, string roleName)
+        {
+            var jwtSettings = _configuration.GetSection("JwtOptions");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["SigningKey"]);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Role, roleName)
+                }),
+                Expires = DateTime.UtcNow.AddSeconds(double.Parse(jwtSettings["ExpirationSeconds"])),
+                Issuer = jwtSettings["Issuer"],
+                Audience = jwtSettings["Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
