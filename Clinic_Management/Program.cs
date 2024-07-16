839c6cef32b893342cf4bafb58f966199425ee53
TranHaiBang12 <133078395+TranHaiBang12@users.noreply.github.com>
2024-07-17 00:43:18 +0700

    update

--- Clinic_Management/Program.cs
+++ Clinic_Management/Program.cs
@@ -1,9 +1,12 @@
-//using Clinic_Management.Models;
+﻿//using Clinic_Management.Models;
 using Clinic_Management.Models;
 using Clinic_Management.Services;
 using Clinic_Management.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Configuration;
+using System.Text;
 
 namespace Clinic_Management
 {
@@ -22,13 +25,48 @@ namespace Clinic_Management
 
             //builder.Services.AddDbContext<G1_PRJ_DBContext>(option =>
             //option.UseSqlServer(configuration.GetConnectionString("MyCnn")));
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            var jwtSettings = configuration.GetSection("JwtOptions");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["SigningKey"]);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("StaffPolicy", policy => policy.RequireRole("Bác sĩ", "Lễ tân"));
+                options.AddPolicy("PatientPolicy", policy => policy.RequireRole("Bệnh nhân"));
+            });
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -37,11 +75,11 @@ namespace Clinic_Management
 
             app.UseRouting();
 
+            app.UseSession();
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapControllers();
             app.MapRazorPages();
-
             app.MapGet("/", context =>
             {
                 context.Response.Redirect("/Index");
@@ -49,7 +87,7 @@ namespace Clinic_Management
             });
 
             app.Run();
-        }
 
+        }
     }
 }
