a3a12db1687fb62d1a6baf407b73035cf990e150
donaldDTrumppp <qtrung1412003@gmail.com>
2024-07-20 04:37:17 +0700

    add reset password

--- Clinic_Management/Utils/JwtMiddleware.cs
+++ Clinic_Management/Utils/JwtMiddleware.cs
@@ -12,6 +12,7 @@ namespace Clinic_Management.Utils
             "/Authentication/Login",
             "/Authentication/Register",
             "/Authentication/ConfirmEmail",
+            "/Authentication/ForgotPassword",
             "/Authentication/ResetPassword",
             "/Authentication/Logout",
             "/Index",
